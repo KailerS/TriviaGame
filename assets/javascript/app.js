10e98d3452f1e14387d74f128a89824e62fb1771
KailerS <KailerS@users.noreply.github.com>
2019-09-16 17:10:04 -0600

    Added some changes to html and inserted all my question objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,53 @@
-alert("This works");
+var timerStart = 35;
+var questions = [{
+    question: "Who is John 117",
+    answers: ["The Arbiter", "Master Chief", "Sgt. Johnson", "Captain Keys"],
+    correctAnswer: "Master Chief",
+    image: "assets/images/toystory.gif",
+}, {
+    question: "What is the name of the ship that the series starts aboard?",
+    answers: ["", "", "", ""],
+    correctAnswer: "",
+    image: "",
+}, {
+    question: "",
+    answers: ["", "", "", ""],
+    correctAnswer: "",
+    image: "",
+}, {
+    question: "",
+    answers: ["", "", "", ""],
+    correctAnswer: "",
+    image: "",
+}, {
+    question: "",
+    answers: ["", "", "", ""],
+    correctAnswer: "",
+    image: "",
+}, {
+    question: "",
+    answers: ["", "", "", ""],
+    correctAnswer: "",
+    image: "",
+}, {
+    question: "",
+    answers: ["", "", "", ""],
+    correctAnswer: "",
+    image: "",
+}, {
+    question: "",
+    answers: ["", "", "", ""],
+    correctAnswer: "",
+    image: "",
+}, {
+    question: "",
+    answers: ["", "", "", ""],
+    correctAnswer: "",
+    image: "",
+}, {
+    question: "",
+    answers: ["", "", "", ""],
+    correctAnswer: "",
+    image: "",
+}];
+
