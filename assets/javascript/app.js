1cb8993eb227ea18d1ba34788de3dafc5403c37e
KailerS <KailerS@users.noreply.github.com>
2019-09-16 17:49:03 -0600

    Adding pictures and questions only 3 left

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,39 @@
 var timerStart = 35;
 var questions = [{
-    question: "Who is John 117",
+    question: "Who is John 117?",
     answers: ["The Arbiter", "Master Chief", "Sgt. Johnson", "Captain Keys"],
     correctAnswer: "Master Chief",
-    image: "assets/images/toystory.gif",
+    image: "assets/images/masterChief.jpg",
 }, {
     question: "What is the name of the ship that the series starts aboard?",
-    answers: ["", "", "", ""],
-    correctAnswer: "",
-    image: "",
+    answers: ["The Pillar of Autumn", "Dawn Under Heaven", "Endeavor", "Hannibal"],
+    correctAnswer: "The Pillar of Autumn",
+    image: "assets/images/unscPillarofAutumn.png",
 }, {
-    question: "",
-    answers: ["", "", "", ""],
-    correctAnswer: "",
-    image: "",
+    question: "What does UNSC stand for?",
+    answers: ["United Nations Security Council", "United Nations Special Corps", "Ultimate Non Sense Center", "United Nations Space Command"],
+    correctAnswer: "United Nations Space Command",
+    image: "assets/images/unscLogo.jpg",
 }, {
-    question: "",
-    answers: ["", "", "", ""],
-    correctAnswer: "",
-    image: "",
+    question: "Who is the leader of Noble Team in Halo: Reach?",
+    answers: ["Kat-320", "Emile-239", "Carter-259", "Spartan-312"],
+    correctAnswer: "Carter-259",
+    image: "assets/images/carter.jpg",
 }, {
-    question: "",
-    answers: ["", "", "", ""],
-    correctAnswer: "",
-    image: "",
+    question: "Which of the following is not one of the Prophets in Halo 2?",
+    answers: ["Mercy", "Truth", "Salvation", "Regret"],
+    correctAnswer: "Salvation",
+    image: "assets/images/prophets.png",
 }, {
-    question: "",
-    answers: ["", "", "", ""],
-    correctAnswer: "",
-    image: "",
+    question: "Who is the main enemy fought throughout the campaign?",
+    answers: ["Flood", "Covenant", "ODST", "Spartans"],
+    correctAnswer: "Covenant",
+    image: "assets/images/covenant.jpg",
 }, {
-    question: "",
-    answers: ["", "", "", ""],
-    correctAnswer: "",
-    image: "",
+    question: "What is another name for The Type-47 Ultra-Heavy Assault Platform?",
+    answers: ["Scarab", "Wraith", "Scorpion", "Banshee"],
+    correctAnswer: "Scarab",
+    image: "assets/images/scarab.jpg",
 }, {
     question: "",
     answers: ["", "", "", ""],
@@ -50,4 +50,5 @@ var questions = [{
     correctAnswer: "",
     image: "",
 }];
+
 
